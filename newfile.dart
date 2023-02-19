eb793e896dce2cc56c5f85817837c8c0a50b1617
2023rahabu <uwamahorora@gmail.com>
2023-02-19 17:14:06 +0200

    change

--- newfile.dart
+++ newfile.dart
@@ -10,7 +10,7 @@ Please Select Operation you wish to perform
 1. Addition
 2.Substraction
 3.Multiplication
-4.Quotient
+4.Quotientt
 Your choice? ''';
   print(message);
   int userChoice = int.parse(stdin.readLineSync()!);
